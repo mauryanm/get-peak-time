f085188bfa68bc98e1ae7c600c0b57d663278b63
Ajay Maurya <ajaymaurya.it@gmail.com>
2023-07-04 12:51:59 +0530

    change void method

--- get-peak-time/src/main/java/com/example/peaktimer/PeakTimer.java
+++ get-peak-time/src/main/java/com/example/peaktimer/PeakTimer.java
@@ -13,7 +13,46 @@ import java.util.Map;
 
 public class PeakTimer {
     public static void getPeakTime(String[] args) {
+        if (args.length > 0) {
+            String dates = args[0];
+            String[] datesList = dates.split(",");
+            if(datesList.length==4){
 
+                String tsd = datesList[0];
+                String ted = datesList[1];
+                String ps = datesList[2];
+                String pe = datesList[3];
+                LocalDateTime tsdf =  LocalDateTime.parse(tsd, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                LocalDateTime tedf =  LocalDateTime.parse(ted, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                long startDate = tsdf.toEpochSecond(ZoneOffset.UTC);
+                long endDate = tedf.toEpochSecond(ZoneOffset.UTC);
+                LocalTime start = LocalTime.parse(ps);
+                LocalTime end = LocalTime.parse(pe);
+                List<List<String>> pt = new ArrayList<>();
+                pt.add(List.of(ps, pe));
+                if (start.compareTo(end) > 0) {
+                    pt.clear();
+                    pt.add(List.of(ps, "23:59:59"));
+                    pt.add(List.of("00:00:00", pe));
+                }
+                Map<String, Integer> result = getPeakTimeDetail(tsd, ted, ps, pe);
+                int peakTotal = (int) Math.ceil(result.get(0));
+                int totalTripTime = (int) Math.ceil(result.get(1));
+
+                System.out.println(result);
+                // System.out.println("Total Time in min: "+ totalTripTime);
+
+                // System.out.println("Peak Time in min: "+peakTotal);
+            }else{
+                System.out.println("Trip date time and peak time not found.");
+                System.out.println("Pass date time formate exaple is ...");
+                System.out.println("2014-01-22 09:00:00,2014-01-25 23:30:00,22:30:00,04:30:00");
+            }
+        }else{
+            System.out.println("Trip date time and peak time not found.");
+            System.out.println("Pass date time formate exaple is ...");
+            System.out.println("2014-01-22 09:00:00,2014-01-25 23:30:00,22:30:00,04:30:00");
+        }
     }
 
     public static Map<String, Integer> getPeakTimeDetail(String startDateS, String endDateS, String pts, String pte) {
